a1b55243c103f1d59fd5fcdf66c1ca653f4cc060
TheMB04 <mbskogrand@gmail.com>
2023-02-09 13:35:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,22 +13,33 @@ url_movies = "https://imdb-api.com/en/API/Top250Movies/k_u4wphtij"
 resultat_movies = req.get(url_movies, headers = { 'User-agent': 'MB' })
 data_movies = resultat_movies.json()
 
-
-@app.route("/")
-def index():
-    navn = "Higher or Lower"
+def get_random_title_left():
     left_random = randint(0, 499)
-    right_random = randint(0, 499)
 
     if left_random >= 250:
         left = data_movies["items"][(499-left_random)]["title"]
     else:
         left = data_shows["items"][left_random]["title"]
 
+    return left
+
+
+def get_random_title_right():
+    right_random = randint(0, 499)
+
     if right_random >= 250:
         right = data_movies["items"][(499-right_random)]["title"]
     else:
         right = data_shows["items"][right_random]["title"]
+
+    return right
+
+
+@app.route("/")
+def index():
+    navn = "Higher or Lower"
+    left = get_random_title_left()
+    right = get_random_title_right()
     return render_template("index.html", navn=navn, left=left, right=right)
 
 app.run(debug=True)
