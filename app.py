a20ec3fe02fc267884c5c2fec05b1275fb58c703
TheMB04 <mbskogrand@gmail.com>
2023-02-28 12:46:12 +0100

    update

--- app.py
+++ app.py
@@ -1,35 +1,44 @@
 from flask import Flask, render_template
 import requests as req
 from random import randint
+import json
 
 
 app = Flask(__name__)
 
 
-url_shows = "https://imdb-api.com/en/API/Top250TVs/k_u4wphtij"
-resultat_shows = req.get(url_shows, headers = { 'User-agent': 'MB' })
-data_shows = resultat_shows.json()
+fil = open("shows.json", encoding = "utf-8")
+data_shows = json.load(fil)
+fil.close()
 
 
-url_movies = "https://imdb-api.com/en/API/Top250Movies/k_u4wphtij"
-resultat_movies = req.get(url_movies, headers = { 'User-agent': 'MB' })
-data_movies = resultat_movies.json()
+fil2 = open("movies.json", encoding = "utf-8")
+data_movies = json.load(fil2)
+fil2.close()
 
 
-def get_random_title():
+#url_shows = "https://imdb-api.com/en/API/Top250TVs/k_u4wphtij"
+#resultat_shows = req.get(url_shows, headers = { 'User-agent': 'MB' })
+#data_shows = resultat_shows.json()
+
+
+#url_movies = "https://imdb-api.com/en/API/Top250Movies/k_u4wphtij"
+#resultat_movies = req.get(url_movies, headers = { 'User-agent': 'MB' })
+#data_movies = resultat_movies.json()
+
+
+def get_random_id():
     random = randint(0, 499)
     if random >= 250:
-        title = data_movies["items"][(499-random)]["title"]
+        title = data_movies["items"][(499-random)]
     else:
-        title = data_shows["items"][random]["title"]
+        title = data_shows["items"][random]
     return title
 
 
 def get_poster(title):
-    url_poster = f"https://imdb-api.com/en/API/SearchSeries/k_u4wphtij/{title}"
-    resultat_poster = req.get(url_poster, headers = { 'User-agent': 'MB' })
-    data_poster = resultat_poster.json()
-    return data_poster["results"][0]["image"]
+    return title["image"]
+
 
 #@app.route("/")
 def home():
@@ -38,24 +47,27 @@ def home():
 
 @app.route("/")
 def index():
-    #left = get_random_title()
-    #right = get_random_title()
-    #left_poster = get_poster(left)
-    #right_poster = get_poster(right)
-    left = "The Dark Knight"
-    right = "The Wolf of Wall Street"
-    left_poster = "/static/test_bilder/The Dark Knight.jpg"
-    right_poster = "/static/test_bilder/The Wolf of Wall Street.jpg"
-    return render_template("index.html", left=left, right=right, left_poster=left_poster, right_poster=right_poster)
+    left = get_random_id()
+    right = get_random_id()
+    left_title = left["title"]
+    right_title = right["title"]
+    left_poster = left["image"]
+    right_poster = right["image"]
+    #left = "The Dark Knight"
+    #right = "The Wolf of Wall Street"
+    #left_poster = "/static/test_bilder/The Dark Knight.jpg"
+    #right_poster = "/static/test_bilder/The Wolf of Wall Street.jpg"
+    return render_template("index.html", left_title=left_title, right_title=right_title, left_poster=left_poster, right_poster=right_poster)
 
 
 @app.route("/id")
 def id():
-    left = right
+    left_title = right_title
     left_poster = right_poster
-    right = get_random_title()
-    right_poster = get_poster(right)
-    return render_template("index.html", left=left, left_poster=left_poster, right=right, right_poster=right_poster)
+    right = get_random_id()
+    right_title = right["title"]
+    right_poster = right["image"]
+    return render_template("index.html", left_title=left_title, left_poster=left_poster, right_title=right_title, right_poster=right_poster)
 
 
 @app.route("/tap")
