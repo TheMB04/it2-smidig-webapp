476688bb8cb07e4593d5ad2cce2b9a439961b1cf
TheMB04 <mbskogrand@gmail.com>
2023-03-09 11:01:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,9 @@ def get_random_id():
 
 score = 0
 highscore = 0
+highscore_rating = 0
+highscore_popularity = 0
+highscore_release = 0
 
 right_g = 0
 right_rating_g = 0
@@ -58,6 +61,7 @@ def index(game):
         global right_g
         global left_rating_g
         global right_rating_g
+        global highscore
 
         left = get_random_id()
         right = get_random_id()
@@ -71,21 +75,35 @@ def index(game):
         if game == "rating":
             left_rating = left["imDbRating"]
             left_rating_g = left_rating
+
             right_rating = right["imDbRating"]
             right_rating_g = right_rating
+
             name = "Which is rated higher?"
+
+            highscore = highscore_rating
+
         elif game == "popularity":
             left_rating = left["imDbRatingCount"]
             left_rating_g = left_rating
+
             right_rating = right["imDbRatingCount"]
             right_rating_g = right_rating
+
             name = "Which is more popular?"
+
+            highscore = highscore_popularity
+
         else:
             left_rating = left["year"]
             left_rating_g = left_rating
+
             right_rating = right["year"]
             right_rating_g = right_rating
+
             name = "Which is the newer release?"
+
+            highscore = highscore_release
 
         return render_template("index.html", left_title=left_title, right_title=right_title, left_poster=left_poster, right_poster=right_poster, score=score, left_rating=left_rating, highscore=highscore, game=game, name=name)
     except:
@@ -99,8 +117,14 @@ def id(game, score, id):
         global right_rating_g
         global right_g
         global highscore
+        global highscore_rating
+        global highscore_popularity
+        global highscore_release
+
         highscore = int(highscore)
         score = int(score)
+        left_rating_g = int(left_rating_g)
+        right_rating_g = int(right_rating_g)
 
         if right_rating_g > left_rating_g and id == "left":
             random_id = get_random_id()
@@ -112,8 +136,6 @@ def id(game, score, id):
             return render_template("tap.html", highscore=highscore, score=score, background=background, game=game)
         else:
             score += 1
-            if score > highscore:
-                highscore = score
 
             left = right_g
             left_title = left["title"]
@@ -128,21 +150,41 @@ def id(game, score, id):
             if game == "rating":
                 left_rating = left["imDbRating"]
                 left_rating_g = left_rating
+
                 right_rating = right["imDbRating"]
                 right_rating_g = right_rating
+
                 name = "Which is rated higher?"
+
+                if score > highscore_rating:
+                    highscore_rating = score
+                highscore = highscore_rating
+
             elif game == "popularity":
                 left_rating = left["imDbRatingCount"]
                 left_rating_g = left_rating
+
                 right_rating = right["imDbRatingCount"]
                 right_rating_g = right_rating
+
                 name = "Which is more popular?"
+
+                if score > highscore_popularity:
+                    highscore_popularity = score
+                highscore = highscore_popularity
+
             else:
                 left_rating = left["year"]
                 left_rating_g = left_rating
+
                 right_rating = right["year"]
                 right_rating_g = right_rating
+
                 name = "Which is the newer release?"
+
+                if score > highscore_release:
+                    highscore_release = score
+                highscore = highscore_release
             
             return render_template("index.html", left_title=left_title, left_poster=left_poster, right_title=right_title, right_poster=right_poster, score=score, left_rating=left_rating, highscore=highscore, game=game, name=name)
     except:
